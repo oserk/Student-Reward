12b3d32960c9bf1407132beb2ec4b2c20bc77207
Oğuzhan Tumantozlu <63979453+oserk@users.noreply.github.com>
2022-04-25 19:55:59 +0300

    scripts added

--- scripts/2.use-contract.sh
+++ scripts/2.use-contract.sh
@@ -1,45 +1,16 @@
 #!/usr/bin/env bash
-
-# exit on first error after this point to avoid redeploying with successful build
 set -e
 
-echo
-echo ---------------------------------------------------------
-echo "Step 0: Check for environment variable with contract name"
-echo ---------------------------------------------------------
-echo
-
 [ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable" && exit 1
-[ -z "$CONTRACT" ] || echo "Found it! \$CONTRACT is set to [ $CONTRACT ]"
 
 echo
+echo 
+echo 'About to call start() on the contract'
+echo near call \$CONTRACT start '{}' --account_id \$CONTRACT
 echo
-echo ---------------------------------------------------------
-echo "Step 1: Call 'view' functions on the contract"
+echo \$CONTRACT is $CONTRACT
+echo 'Create student profile'
+near call $CONTRACT createStudent '{ "name": "YourName", "yearOfbirth": yourBirthdayYear, "country": "YourCountry", "computerEquipment": "yourNeededEquipment", "pocketMoney": putNumber, "neededVote": putNumberOfNeededVote }' --accountId $CONTRACT
 echo
-echo "(run this script again to see changes made by this file)"
-echo ---------------------------------------------------------
-echo
-
-near view $CONTRACT helloWorld
-
-echo
-echo
-
-near view $CONTRACT read '{"key":"some-key"}'
-
-echo
-echo
-echo ---------------------------------------------------------
-echo "Step 2: Call 'change' functions on the contract"
-echo ---------------------------------------------------------
-echo
-
-# the following line fails with an error because we can't write to storage without signing the message
-# --> FunctionCallError(HostError(ProhibitedInView { method_name: "storage_write" }))
-# near view $CONTRACT write '{"key": "some-key", "value":"some value"}'
-near call $CONTRACT write '{"key": "some-key", "value":"some value"}' --accountId $CONTRACT
-
-echo
-echo "now run this script again to see changes made by this file"
-exit 0
+echo 'Vote'
+near call $CONTRACT timeToVote '{ "id": putStudentId }' --accountId $CONTRACT --deposit $1
