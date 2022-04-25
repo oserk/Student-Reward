3953412d7ba4e690a35cae92e325a4b0665cbb71
Oğuzhan Tumantozlu <63979453+oserk@users.noreply.github.com>
2022-04-25 19:59:57 +0300

    Update 3.cleanup.sh

--- scripts/3.cleanup.sh
+++ scripts/3.cleanup.sh
@@ -2,25 +2,14 @@
 
 # exit on first error after this point to avoid redeploying with successful build
 set -e
-
-echo
-echo ---------------------------------------------------------
-echo "Step 0: Check for required environment variables"
-echo ---------------------------------------------------------
-echo
-
 [ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable" && exit 1
-[ -z "$BENEFICIARY" ] && echo "Missing \$BENEFICIARY environment variable" && exit 1
-[ -z "$CONTRACT" ] || echo "Found it! \$CONTRACT is set to [ $CONTRACT ]"
-[ -z "$BENEFICIARY" ] || echo "Found it! \$BENEFICIARY is set to [ $BENEFICIARY ]"
-
 echo
 echo
 echo ---------------------------------------------------------
-echo "Step 1: Delete $CONTRACT, setting $BENEFICIARY as beneficiary"
+echo "Step 1: Delete $CONTRACT
 echo ---------------------------------------------------------
 echo
-near delete $CONTRACT $BENEFICIARY
+near delete $CONTRACT 
 
 echo
 echo ---------------------------------------------------------
